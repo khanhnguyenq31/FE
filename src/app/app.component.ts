363aba6252ba74bdb45ed53d6b480e7abd670b85
Khoa <anhkhoaoki@gmail.com>
2024-10-04 18:39:10 +0700

    akhoa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { CommonModule } from '@angular/common';
+import { SongsectionComponent } from "./components/songsection/songsection.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, CommonModule],
+  imports: [RouterOutlet, HomeComponent, CommonModule, SongsectionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
