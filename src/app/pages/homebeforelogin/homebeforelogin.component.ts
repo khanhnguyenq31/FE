32bb17bce0ccddfdc1f36a8fa04e12d4af45a2ab
sontapcode <156274946+tuitensonne@users.noreply.github.com>
2024-10-04 21:55:33 +0700

    Update homebeforelogin.component.ts

--- src/app/pages/homebeforelogin/homebeforelogin.component.ts
+++ src/app/pages/homebeforelogin/homebeforelogin.component.ts
@@ -2,14 +2,34 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { SignupComponent } from '../signup/signup.component';
 import { LoginComponent } from '../login/login.component';
+import { SongsectionComponent } from "../../components/songsection/songsection.component";
+import { CommonModule } from '@angular/common';
+import { SidebarsectionComponent } from "../../components/sidebarsection/sidebarsection.component"; // Import CommonModule
 
 @Component({
   selector: 'app-homebeforelogin',
   standalone: true,
-  imports: [RouterLink, SignupComponent, LoginComponent],
+  imports: [RouterLink, SignupComponent, LoginComponent, SongsectionComponent, CommonModule, SidebarsectionComponent],
   templateUrl: './homebeforelogin.component.html',
-  styleUrl: './homebeforelogin.component.css'
+  styleUrls: ['./homebeforeloginstyle/homebeforelogin.component.css', 
+    './homebeforeloginstyle/header.css', 
+    './homebeforeloginstyle/sidebar.css', 
+    './homebeforeloginstyle/main.css',
+    './homebeforeloginstyle/notification.css', 
+    './homebeforeloginstyle/footer.css']
 })
 export class HomebeforeloginComponent {
-
+  hiddenFlag = true;
+  iddenFlag = true;
+  
+  // open the notification
+  open() {
+    this.hiddenFlag = false;
+  }
+  
+  // close the notification
+  close() {
+    this.hiddenFlag = true;
+  }
+  
 }
