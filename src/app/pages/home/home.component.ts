3188d2594b70c5e155bd9141bc6ae885bd94fb78
son.tran22 <son.trannam.bku@gmail.com>
2024-10-22 20:25:44 +0700

    Make chagnes on homebeforelogin and home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,14 +2,25 @@ import { Component } from '@angular/core';
 import { SongsectionComponent } from '../../components/songsection/songsection.component';
 import { SidebarsectionComponent } from '../../components/sidebarsection/sidebarsection.component';
 import { RouterLink } from '@angular/router';
-
+import { ProfilemenuComponent } from '../../components/profilemenu/profilemenu.component';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [SongsectionComponent, SidebarsectionComponent, RouterLink],
+  imports: [SongsectionComponent, SidebarsectionComponent, RouterLink , ProfilemenuComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  hiddenFlag = true;
+  iddenFlag = true;
+  
+  // open the notification
+  open() {
+    this.hiddenFlag = false;
+  }
+  
+  // close the notification
+  close() {
+    this.hiddenFlag = true;
+  }
 }
