c517ae1549c1550c7273ea22c405f69a893c5d0f
khanhnguyenq31 <qkn3174@gmail.com>
2024-12-23 22:48:27 +0700

    mnupdate

--- src/app/artist/songs/songs.component.ts
+++ src/app/artist/songs/songs.component.ts
@@ -6,24 +6,43 @@ import { SidebarsectionComponent } from '../../components/sidebarsection/sidebar
 import { ProfilemenuComponent } from '../../components/profilemenu/profilemenu.component';
 import { ArtistSongService } from '../../services/artist/song.service';
 import { DataStorageService } from '../../data-storage.service';
+import { UserDetailService } from '../../services/user-detail.service';
+import { ApiResponse } from '../../responses/api.response';
+import { TokenService } from '../../services/token.service';
 @Component({
   selector: 'app-songs',
   standalone: true,
   imports: [RouterLink, SidebarsectionComponent,CommonModule, FormsModule],
   templateUrl: './songs.component.html',
-  styleUrl: './songs.component.css',
+  styleUrls: ['./songs.component.css'],
 })
 export class SongsComponent implements OnInit {
   songs: any[] = []; // Giữ nguyên kiểu any[]
   isLoading = true;
+  username: string = '';
+  imageUrl: string | null = null;
 
   constructor(
     private artistSongService: ArtistSongService,
-    private dataService: DataStorageService
+    private dataService: DataStorageService,
+    private userDetailService: UserDetailService,
+    private tokenSvc: TokenService
   ) {}
 
   ngOnInit(): void {
     this.loadSongs();
+    const token = this.tokenSvc.getToken();
+        this.userDetailService.getUserDetails(token).subscribe({
+          next: (response: ApiResponse) => {
+            if (response.status === 'OK') {
+              this.username = response.data.username;
+              this.imageUrl = response.data.image_url; // Lưu trữ URL hình ảnh
+            }
+          },
+          error: (error) => {
+            console.error('Error fetching user details:', error);
+          }
+        });
   }
 
   loadSongs(): void {
